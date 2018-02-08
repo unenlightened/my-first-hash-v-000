69e3b9566a766c3d318855266a4e97db91835eaf
Chely Ho <adept_inept@yahoo.com>
2018-02-08 06:04:36 +0000

    automatically backed up by learn

--- my_first_hash.rb
+++ my_first_hash.rb
@@ -8,6 +8,7 @@ def shipping_manifest
   # fill that hash with key/value pairs that describe the following information:
   # We have 5 whale bone corsets, 2 porcelain vases and 3 oil paintings
 
+  the_manifest = {}
 
 end
 
@@ -19,7 +20,7 @@ def retrieval
                  }
 
   # Look up the value of the "oil paintings" key in the shipping_manifest hash below
-
+  
 end
 
 def adding
